b75dec2b40c5195038f5d189dc81e178d12be90c
remintroy <reminremin01@gmail.com>
2023-04-06 19:50:51 +0530

    Token Update

--- authServer/src/admin/controller/index.ts
+++ authServer/src/admin/controller/index.ts
@@ -7,10 +7,11 @@ export const userSignIn = async (req: RequestDefention, res: Response) => {
     const { accessToken, refreshToken, email, photoURL, name } = await signInUserWithPassword(req.body);
     let currentdate = new Date();
     let next3months = new Date(currentdate.setMonth(currentdate.getMonth() + 3));
-    res.cookie("refreshToken", refreshToken, { httpOnly: true, secure: true, expires: next3months });
+    res.cookie("suRefreshToken", refreshToken, { httpOnly: true, secure: true, expires: next3months });
     res.send({ accessToken, email, photoURL, name });
   } catch (error) {
     res.status(error?.code ? error.code : 500);
     res.send(error);
   }
 };
+
