7c529f254e3fd24c28b56b4952c02ab3941a0839
remintroy <reminremin01@gmail.com>
2023-04-13 00:40:55 +0530

    INITIAL COMMIT

---
+++ commonServices/src/email/index.ts
@@ -0,0 +1,29 @@
+import nodeMailer from "nodemailer";
+import sendOtp from "./sendOtp";
+
+const config = (AccountEmail: string, password: string) => {
+  // creates a reusable transport instance
+  const transport = nodeMailer.createTransport({
+    host: "smtp.zoho.in",
+    port: 465,
+    secure: true,
+    auth: {
+      user: AccountEmail,
+      pass: password,
+    },
+  });
+
+  // reutrns available functions
+  return {
+    // Sends otp to user with default otp template for authentication
+    sendOtp: async (toEmail: string, otp: string) => {
+      try {
+        await sendOtp(transport, AccountEmail, toEmail, otp);
+      } catch (error) {
+        throw error;
+      }
+    },
+  };
+};
+
+export default config;
---
+++ commonServices/src/index.ts
@@ -0,0 +1,7 @@
+import emailObj from "./email";
+
+export default {
+  getEmail: emailObj,
+};
+
+export const getEmail = emailObj;
