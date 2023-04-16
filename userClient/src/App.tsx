1ff18cda9674c0007ad0c7eec0c8b17985843f12
remintroy <reminremin01@gmail.com>
2023-04-16 16:37:05 +0530

    Make complete Client App Scss Update

--- userClient/src/App.tsx
+++ userClient/src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import NavBar from "./components/NavBar";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home";
--- userClient/src/main.tsx
+++ userClient/src/main.tsx
@@ -1,7 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./index.scss";
 import { Provider } from "react-redux";
 import Store from "./redux/store";
 
