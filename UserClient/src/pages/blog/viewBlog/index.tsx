53a82bc54aba1d98c790d25343f49bf3febc2a32
Remin T Roy <reminremin01@gmail.com>
2023-05-16 01:54:35 +0530

    [User Client + BugFix] Updates on blogView + Disabled blog handling

--- UserClient/src/pages/blog/viewBlog/index.tsx
+++ UserClient/src/pages/blog/viewBlog/index.tsx
@@ -1,7 +1,7 @@
 import "./style.scss";
 import { useEffect, useState } from "react";
 import { Link, useParams } from "react-router-dom";
-import { Avatar, Box, Container, Flex, Grid, Image, Skeleton, Text } from "@mantine/core";
+import { Alert, Avatar, Box, Container, Divider, Flex, Grid, Image, Skeleton, Text } from "@mantine/core";
 import { Prism } from "@mantine/prism";
 import parse from "html-react-parser";
 import { QuillDeltaToHtmlConverter } from "quill-delta-to-html";
@@ -15,13 +15,13 @@ import ErrorImage from "../../../assets/error.png";
 import BlogActionButtonComponent from "../../../components/blog/blogActions";
 import { NavigationProgress } from "@mantine/nprogress";
 import ReportBlogComponet from "../../../components/blog/reportBlog";
-import BlogCardComponent from "../../../components/profile/blogs/blogCard";
+import BlogCardNormalComponent from "../../../components/profile/blogs/blogCardNormal";
 
 const BlogViewPage = () => {
   const user = useAppSelector((state) => state.user.data);
   const { id: blogId } = useParams();
   const { data: blogData, isError, error } = useGetBlogQuery({ blogId });
-  const { data: blogsListData, isLoading, isFetching } = useGetBlogsForHomeQuery({});
+  const { data: blogsListData } = useGetBlogsForHomeQuery({});
 
   const {
     data: authorData,
@@ -61,6 +61,12 @@ const BlogViewPage = () => {
           <Grid.Col span={12}>
             <>
               <h1 style={{ marginBottom: 0 }}>{blogData?.title}</h1>
+              {blogData?.disabled && (
+                <Alert my={20} icon={<IconExclamationCircle />} color="red" variant="outline">
+                  This blog has been disabled due to discovery of issues with the content and will be hidden for other
+                  users
+                </Alert>
+              )}
               <Text color="dimmed" size={"sm"}>
                 Created at : {new Date(blogData?.createdAt).toDateString()}
               </Text>
@@ -90,7 +96,6 @@ const BlogViewPage = () => {
                       <>{user && <FollowButtonComponent userId={authorData?.uid} />}</>
                     )}
                   </Flex>
-
                   <BlogActionButtonComponent blogId={blogId} />
                 </Flex>
               </Box>
@@ -100,30 +105,36 @@ const BlogViewPage = () => {
                 {blogData?.subtitle}
               </Text>
               <div className="body">{blogDataToShow ? blogDataToShow : <Skeleton w="100%" h={100} />}</div>
-              <div className="line"></div>
-              <ReportBlogComponet blogId={blogId} />
+              <Divider my={20} />
               <CommentSectionComponent blogId={blogId} skip={!blogData} />
-              <Box>
-                <h2>Related Blogs</h2>
-                <Grid gutter={"lg"}>
-                  {blogsListData?.map((blog: any) => {
-                    return (
-                      <BlogCardComponent span={6} key={blog?.blogId} blog={blog} userId={blog?.author} />
-                    );
-                  })}
-                  {blogsListData?.length === 0 && <h3 style={{ padding: 10 }}>Yay.. There is nothing private here</h3>}
-                </Grid>
-              </Box>
+              {!blogData?.disabled && (
+                <Box my={20}>
+                  <ReportBlogComponet blogId={blogId} />
+                </Box>
+              )}
+              {!blogData?.disabled && (
+                <Box>
+                  <h2>Related Blogs</h2>
+                  <Grid gutter={"lg"}>
+                    {blogsListData?.map((blog: any) => {
+                      return <BlogCardNormalComponent span={12} key={blog?.blogId} blog={blog} userId={blog?.author} />;
+                    })}
+                    {blogsListData?.length === 0 && (
+                      <h3 style={{ padding: 10 }}>Yay.. There is nothing private here</h3>
+                    )}
+                  </Grid>
+                </Box>
+              )}
             </>
           </Grid.Col>
         </Grid>
       )}
       {isError && (
         <>
-          <Image mt={50} src={ErrorImage} maw={500} mx={"auto"} />
-          <Flex align={"center"} gap={10} justify={"center"}>
-            <IconExclamationCircle size={"30px"} />{" "}
-            <Text fz={"30px"} fw={"bold"}>
+          <Image mt={100} src={ErrorImage} maw={500} mx={"auto"} />
+          <Flex align={"start"} gap={10} justify={"center"}>
+            <IconExclamationCircle style={{ marginTop: 8 }} size={"30px"} />{" "}
+            <Text mt={0} sx={{ lineHeight: 1.5 }} fz={"30px"} fw={"bold"}>
               {error?.data?.error ?? "Oops something went wrong !"}
             </Text>
           </Flex>
